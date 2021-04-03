81c95c09bc162a9faf6ad3b9ed17feeacacc2a43
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-04-03 15:34:56 -0700

    script running correctly, final commit for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,16 @@
 # import dependencies
 import os
 import csv
+import sys
 
 # path to collect data from the Resources folder
 csv_file = os.path.join('PyBank','Resources', 'budget_data.csv')
 
-
-total_profit_losses = 0
+# set global variables to be used in for loop
+total = 0
 previous = 867884
+
+# open/read csv file
 with open(csv_file, 'r') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -19,40 +22,36 @@ with open(csv_file, 'r') as csvfile:
     csvfile.seek(0)
     header = next(csvreader)
 
+    # set empty lists to write dates and change in profit/loss to
     change = []
     dates = []
-    for row in csvreader:  
+
+    # start looping through csv file
+    for row in csvreader:
+        # add months to dates list  
         dates.append(row[0])
-        total_profit_losses += int(row[1])
+
+        # calculate sum of profits/losses
+        total += int(row[1])
+
+        # add deltas to change list, starting with delta of 0 by using previous = 867884
         change.append(int(row[1]) - previous)
         previous = int(row[1])
-
-
-    changes_by_month = dict(zip(dates, change))
-
-    print("resulting dictionary is :" + str(changes_by_month))
-    
-    #def budget_data():
-    greatest_increase = max(change)
-    print(greatest_increase)
-
-
-    #for row in csvreader:
-   
-    if int(row[1]) == greatest_increase:
-        print(f"Greatest Increase in Profits: {row[0]} ({greatest_increase}")
-       
-    
+ 
+    # use lists created by loop to perform calculations
     av_change = sum(change)/(num_of_months - 1)
-    print(av_change)
-    #greatest_increase = max(change)
-    #if row[1]
-    #print(greatest_increase)
-    #greatest_decrease = min(change)
-    #print(greatest_decrease)
-    #print(f"Total: ${total_profit_losses}")
+    average = "{:.2f}".format(av_change)
+    max_increase = max(change)
+    max_increase_position = change.index(max_increase)
+    max_decrease = min(change)
+    max_decrease_position = change.index(max_decrease)
 
-    #print(num_of_months)
-    #print(av_change) #format this
-    
+    # print to terminal
+    print(f"Financial Analysis\n----------------------------\nTotal Months: {num_of_months}\nTotal: ${total}\nAverage Change: ${average}\nGreatest Increase in Profits: {dates[max_increase_position]} (${max_increase})\nGreatest Decrease in Profits: {dates[max_decrease_position]} (${max_decrease})")
 
+    # write to txt file
+    sys.stdout = open('PyBank/Analysis/financial_analysis.txt', 'w')
+
+    print(f"Financial Analysis\n----------------------------\nTotal Months: {num_of_months}\nTotal: ${total}\nAverage Change: ${average}\nGreatest Increase in Profits: {dates[max_increase_position]} (${max_increase})\nGreatest Decrease in Profits: {dates[max_decrease_position]} (${max_decrease})")
+
+    sys.stdout.close()
