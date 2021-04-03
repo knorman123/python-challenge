1575bb9746a11b6e782bacbd7f6248d32edb6832
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-04-03 15:00:39 -0700

    created dictionary of months with deltas, so key can be printed for max and min deltas

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,6 @@ csv_file = os.path.join('PyBank','Resources', 'budget_data.csv')
 
 
 total_profit_losses = 0
-change = 0
 previous = 867884
 with open(csv_file, 'r') as csvfile:
 
@@ -20,18 +19,40 @@ with open(csv_file, 'r') as csvfile:
     csvfile.seek(0)
     header = next(csvreader)
 
-    for row in csvreader:
+    change = []
+    dates = []
+    for row in csvreader:  
+        dates.append(row[0])
         total_profit_losses += int(row[1])
-        change += int(row[1]) - previous
-        print(f"change {change}")
+        change.append(int(row[1]) - previous)
         previous = int(row[1])
-        print(f"previous {previous}")
+
+
+    changes_by_month = dict(zip(dates, change))
+
+    print("resulting dictionary is :" + str(changes_by_month))
     
-    av_change = change/(num_of_months - 1)
-    print(f"Total: ${total_profit_losses}")
-    print(change)
-    print(previous)
-    print(num_of_months)
-    print(av_change) #format this
+    #def budget_data():
+    greatest_increase = max(change)
+    print(greatest_increase)
+
+
+    #for row in csvreader:
+   
+    if int(row[1]) == greatest_increase:
+        print(f"Greatest Increase in Profits: {row[0]} ({greatest_increase}")
+       
+    
+    av_change = sum(change)/(num_of_months - 1)
+    print(av_change)
+    #greatest_increase = max(change)
+    #if row[1]
+    #print(greatest_increase)
+    #greatest_decrease = min(change)
+    #print(greatest_decrease)
+    #print(f"Total: ${total_profit_losses}")
+
+    #print(num_of_months)
+    #print(av_change) #format this
     
 
