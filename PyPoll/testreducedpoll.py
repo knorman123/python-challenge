bead603ce1db5008b0e56bdcd1afbbf61dc4b01d
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-04-02 22:25:14 -0700

    candidate stats function running successfully.  need to add print out of percent and output winner

--- PyPoll/testreducedpoll.py
+++ PyPoll/testreducedpoll.py
@@ -14,15 +14,34 @@ with open(csv_file, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
     # iterate through file once for the length of the list, in order to get the total number of votes cast
-    total_votes = len(list(csvreader))
-    print(total_votes)
+    total_voters = len(list(csvreader))
+    print(total_voters)
     # start the iteration again for the For Loop calculations
     csvfile.seek(0)
     header = next(csvreader)
-
+    
+    vote_list = []
     candidate_list = []
+    #num_of_candidates = 0
+    #candidate_one_votes = 0
     for row in csvreader:
-        if row[2] not in candidate_list:
-            candidate_list.append(row[2])
+        vote_list.append(row[2])
         
+        for unique in vote_list:
+            if row[2] not in candidate_list:
+                candidate_list.append(row[2])
+    def candidate_stats():
+        num_of_candidates = 0    
+        for votes in candidate_list:
+            total_votes = vote_list.count(candidate_list[num_of_candidates])
+            percent_votes = total_votes/total_voters
+            print(f"{candidate_list[num_of_candidates]}: ({total_votes})" )
+            num_of_candidates += 1
+        
+
+
     print(candidate_list)
+
+    print("Election Results")
+    candidate_stats()
+    print("-----------------")
